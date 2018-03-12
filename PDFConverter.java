443935b1a3a9002d87f586da5f8fd34db054c3ab
V1centR <vicentcdb@gmail.com>
2018-03-12 08:54:53 -0300

    apply psr

--- PDFConverter.java
+++ PDFConverter.java
@@ -1,34 +1,66 @@
-public class ConversorPDF {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
-    // A aplicação foi projetada para tratar arquivos WORD, temos que cuidar para que
-    // alterações não mudem o comportamento, para que não exista reflexo para clientes
-    // legados
-    public String tipoDocumento = "WORD";
+import com.aspose.cells.*;
+import com.aspose.words.PdfCompliance;
+import com.aspose.words.PdfSaveOptions;
+import com.aspose.words.SaveFormat;
 
-    /**
-     * Esse método recebe o como entrada o arquivo que vai ser convertido
-     * para PDF
-     *
-     * @param bytesArquivo
-     * @return
-     */
-    public byte[] converteParaPDF(byte[] bytesArquivo){
-        if(tipoDocumento.equals("WORD")) {
-            InputStream entrada = new ByteArrayInputStream(bytesArquivo);
-            com.aspose.words.Document documentoWord = new com.aspose.words.Document(entrada);
-            ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
-            documentoWord.save(documentoPDF, SaveFormat.PDF);
+public class PDFConverter {
 
-            return documentoPDF.toByteArray();
-        } else {
-            InputStream entrada = new ByteArrayInputStream(bytesArquivo);
-            Workbook workbook = new Workbook(entrada);
-            PdfSaveOptions opcaoSalvar = new PdfSaveOptions();
-            opcaoSalvar.setCompliance(PdfCompliance.PDF_A_1_B);
-            ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
-            workbook.save(documentoPDF, opcaoSalvar);
+	public String tipoDocumento = "";
 
-            return documentoPDF.toByteArray();
-        }
-    }
+	/**
+	 * Método recebe o como entrada o arquivo que vai ser convertido para PDF
+	 *
+	 * @param bytesArquivo
+	 * @return
+	 */
+	public byte[] converteParaPDF(byte[] bytesArquivo) {
+
+		if (tipoDocumento.equals("EXCEL")) {
+
+			return workFile2Excel(bytesArquivo);
+
+		}
+
+		if (tipoDocumento.equals("WORD")) {
+
+			return workFile(bytesArquivo);
+
+		} else {
+
+			InputStream entrada = new ByteArrayInputStream(bytesArquivo);
+			Workbook workbook = new Workbook(entrada);
+			PdfSaveOptions opcaoSalvar = new PdfSaveOptions();
+			opcaoSalvar.setCompliance(PdfCompliance.PDF_A_1_B);
+			ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
+			workbook.save(documentoPDF, opcaoSalvar);
+
+			return documentoPDF.toByteArray();
+		}
+
+	}
+
+	private byte[] workFile(byte[] bytesArquivo) throws Exception {
+
+		InputStream entrada = new ByteArrayInputStream(bytesArquivo);
+		com.aspose.words.Document documentoWord = new com.aspose.words.Document(entrada);
+		ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
+		documentoWord.save(documentoPDF, SaveFormat.PDF);
+
+		return documentoPDF.toByteArray();
+
+	}
+
+	private byte[] workFile2Excel(byte[] bytesArquivo) throws Exception {
+
+		InputStream entrada = new ByteArrayInputStream(bytesArquivo);
+		Workbook asposeCellWb = new Workbook(entrada);
+		ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
+		asposeCellWb.save(documentoPDF, SaveFormat.PDF);
+
+		return documentoPDF.toByteArray();
+	}
 }
