390f1f5f45b062d4fddf388d4f62b8971c742059
matheus\matheus_lima <matheus.nascimento@kainos.com.br>
2018-04-02 11:21:40 -0300

    Refatoração da classe PDFConverter

--- PDFConverter.java
+++ PDFConverter.java
@@ -13,22 +13,46 @@ public class ConversorPDF {
      * @return
      */
     public byte[] converteParaPDF(byte[] bytesArquivo){
-        if(tipoDocumento.equals("WORD")) {
-            InputStream entrada = new ByteArrayInputStream(bytesArquivo);
-            com.aspose.words.Document documentoWord = new com.aspose.words.Document(entrada);
-            ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
-            documentoWord.save(documentoPDF, SaveFormat.PDF);
-
-            return documentoPDF.toByteArray();
-        } else {
-            InputStream entrada = new ByteArrayInputStream(bytesArquivo);
-            Workbook workbook = new Workbook(entrada);
-            PdfSaveOptions opcaoSalvar = new PdfSaveOptions();
-            opcaoSalvar.setCompliance(PdfCompliance.PDF_A_1_B);
-            ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
-            workbook.save(documentoPDF, opcaoSalvar);
-
-            return documentoPDF.toByteArray();
+        if(tipoDocumento.equals("WORD")) {            
+            return wordToPDF(bytesArquivo);
+        } else if(tipoDocumento.equals("EXCEL")) {            
+            return excelToPDF(bytesArquivo);      
+        }else{
+            return null;
         }
+    }
+
+    /**
+     * Esse método recebe o como entrada um arquivo no formato word que vai ser convertido
+     * para PDF
+     * @param bytesArquivo
+     * @return
+     */
+    public byte[] wordToPDF(byte[] bytesArquivo)
+    {
+        InputStream entrada = new ByteArrayInputStream(bytesArquivo);
+        ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();        
+        com.aspose.words.Document documentoWord = new com.aspose.words.Document(entrada);            
+        documentoWord.save(documentoPDF, SaveFormat.PDF);   
+        
+        return documentoPDF.toByteArray(byte[] bytesArquivo);
+    }
+
+    /**
+     * Esse método recebe o como entrada um arquivo no formato excel que vai ser convertido
+     * para PDF
+     * @param bytesArquivo
+     * @return
+     */
+    public byte[] excelToPDF()
+    {
+        InputStream entrada = new ByteArrayInputStream(bytesArquivo);
+        ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();         
+        Workbook workbook = new Workbook(entrada);
+        PdfSaveOptions opcaoSalvar = new PdfSaveOptions();
+        opcaoSalvar.setCompliance(PdfCompliance.PDF_A_1_B);
+        workbook.save(documentoPDF, opcaoSalvar);                
+        
+        return documentoPDF.toByteArray();
     }
 }
