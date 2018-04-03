0d05d5482428c9fedb2199cc85b3234dda46b75e
Dheiler <dheiler@hotmail.com>
2018-04-03 19:59:03 -0300

    Update PDFConverter.java

--- PDFConverter.java
+++ PDFConverter.java
@@ -1,10 +1,5 @@
 public class ConversorPDF {
 
-    // A aplicação foi projetada para tratar arquivos WORD, temos que cuidar para que
-    // alterações não mudem o comportamento, para que não exista reflexo para clientes
-    // legados
-    public String tipoDocumento = "WORD";
-
     /**
      * Esse método recebe o como entrada o arquivo que vai ser convertido
      * para PDF
@@ -13,14 +8,6 @@ public class ConversorPDF {
      * @return
      */
     public byte[] converteParaPDF(byte[] bytesArquivo){
-        if(tipoDocumento.equals("WORD")) {
-            InputStream entrada = new ByteArrayInputStream(bytesArquivo);
-            com.aspose.words.Document documentoWord = new com.aspose.words.Document(entrada);
-            ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
-            documentoWord.save(documentoPDF, SaveFormat.PDF);
-
-            return documentoPDF.toByteArray();
-        } else {
             InputStream entrada = new ByteArrayInputStream(bytesArquivo);
             Workbook workbook = new Workbook(entrada);
             PdfSaveOptions opcaoSalvar = new PdfSaveOptions();
@@ -29,6 +16,5 @@ public class ConversorPDF {
             workbook.save(documentoPDF, opcaoSalvar);
 
             return documentoPDF.toByteArray();
-        }
     }
 }
