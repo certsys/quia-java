5cb14647d07c6d2fc7cc308fedd642b188f115a3
adilsonJunior <adilson.fernandesjr@globo.com>
2018-04-07 16:07:53 -0300

    Add files via upload

--- PDFConverter.java
+++ PDFConverter.java
@@ -1,34 +1,46 @@
 public class ConversorPDF {
 
-    // A aplica√ß√£o foi projetada para tratar arquivos WORD, temos que cuidar para que
-    // altera√ß√µes n√£o mudem o comportamento, para que n√£o exista reflexo para clientes
-    // legados
-    public String tipoDocumento = "WORD";
+	// A aplicaÁ„o foi projetada para tratar arquivos WORD, temos que cuidar para que
+	// alteraÁıes n„o mudem o comportamento, para que n„o exista reflexo para clientes
+	// legados
+	
+	public ConversorPDF( String tipo ){
+		public String tipoDocumento = tipo;
+	}
+	
+	/**
+	 * Esse mÈtodo recebe o como entrada o arquivo que vai ser convertido
+	 * para PDF
+	 *
+	 * @param bytesArquivo
+	 * @return
+	 */
+	public byte[] converteParaPDF(byte[] bytesArquivo) throws Exception{
+		ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
+		
+		if (tipoDocumento.equals("WORD") || tipoDocumento.equals("EXCEL")){
+			try{
+				if(tipoDocumento.equals("WORD")) {
+					InputStream entrada = new ByteArrayInputStream(bytesArquivo);
+					com.aspose.words.Document documentoWord = new com.aspose.words.Document(entrada);
+					documentoWord.save(documentoPDF, SaveFormat.PDF);
 
-    /**
-     * Esse m√©todo recebe o como entrada o arquivo que vai ser convertido
-     * para PDF
-     *
-     * @param bytesArquivo
-     * @return
-     */
-    public byte[] converteParaPDF(byte[] bytesArquivo){
-        if(tipoDocumento.equals("WORD")) {
-            InputStream entrada = new ByteArrayInputStream(bytesArquivo);
-            com.aspose.words.Document documentoWord = new com.aspose.words.Document(entrada);
-            ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
-            documentoWord.save(documentoPDF, SaveFormat.PDF);
+					return documentoPDF.toByteArray();
+				} else {
+					InputStream entrada = new ByteArrayInputStream(bytesArquivo);
+					Workbook workbook = new Workbook(entrada);
+					PdfSaveOptions opcaoSalvar = new PdfSaveOptions();
+					opcaoSalvar.setCompliance(PdfCompliance.PDF_A_1_B);
+					workbook.save(documentoPDF, opcaoSalvar);
 
-            return documentoPDF.toByteArray();
-        } else {
-            InputStream entrada = new ByteArrayInputStream(bytesArquivo);
-            Workbook workbook = new Workbook(entrada);
-            PdfSaveOptions opcaoSalvar = new PdfSaveOptions();
-            opcaoSalvar.setCompliance(PdfCompliance.PDF_A_1_B);
-            ByteArrayOutputStream documentoPDF = new ByteArrayOutputStream();
-            workbook.save(documentoPDF, opcaoSalvar);
-
-            return documentoPDF.toByteArray();
-        }
-    }
+					return documentoPDF.toByteArray();
+				}
+			}catch(Exception e){
+				System.out.println("Erro ao converter o arquivo do tipo" + tipoDocumento);
+			}
+		} else {
+			System.out.println("Tipo de arquivo n„o permitido:" + tipoDocumento);
+			return null;
+		}
+	}
 }
